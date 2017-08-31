1888d13d6d55fec490e5aae6f85ff41f710140e4
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-08-31 08:54:07 +0200

    filtrowanie agregatów

--- SQL dla analityków - dzien3.sql
+++ SQL dla analityków - dzien3.sql
@@ -2,9 +2,58 @@ use AdventureWorks2014
 
 -- wyciêcie fragmentu tekstu
 select 
-	substring(AccountNumber, 1, 2) 
+	substring(AccountNumber, 1, 2) ,
+	case 
+		when AccountNumber is not null then 0
+	end,
+	*
 from 
 	Sales.Customer
+
+
+
+-- podzapytania
+
+select * from 
+(
+	select 
+	substring(AccountNumber, 1, 2) as Prefix,
+	case 
+		when AccountNumber is not null then 0
+	end as Account,
+	PersonId,
+	StoreId
+from 
+	Sales.Customer
+)  as t
+
+go
+
+
+-- CTE (Common Table Expression)
+with myquery as
+(
+select 
+	substring(AccountNumber, 1, 2) as Prefix,
+	case 
+		when AccountNumber is not null then 0
+	end as Account,
+	PersonId,
+	StoreId
+from 
+	Sales.Customer
+)
+
+select * from myquery
+
+
+
+
+
+
+
+
+
 
 
 
